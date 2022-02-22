27700552499e9013ea4d19d532239ac2c8db785c
Everton França <everton.r.franca@gmail.com>
2022-02-22 18:20:36 -0300

    implements conversor bases to decimal

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { base10Converter } from "./utils/base10Converter";
+
+console.log(base10Converter("ff", 16).value);
---
+++ src/utils/base10Converter.js
@@ -0,0 +1,81 @@
+const bigInt = require("big-integer");
+
+function sanitizeNumber(numberString) {
+   return numberString.replace(/[^0-9a-f]/gi, "");
+}
+
+function numberIsValid(numberStringSanitized) {
+   return (
+      numberStringSanitized.length > 0 &&
+      numberStringSanitized.length <= 1000000
+   );
+}
+
+function baseIsValid(numberStringSanitized, base) {
+   if (base > 1 && base <= 16) {
+      const digits = greater9Converter(
+         extracDigits(sanitizeNumber(numberStringSanitized.toString()))
+      );
+      const max = Math.max(...digits);
+      return max >= 0 && max <= base - 1;
+   }
+}
+
+function InputIsEqualSanitized(StringInput, StringSanitized) {
+   return StringInput === StringSanitized;
+}
+
+function extracDigits(numberString) {
+   return numberString.split("");
+}
+
+function allSanitizeNumber(numberString, base) {
+   const numberStringSanitized = sanitizeNumber(numberString);
+   if (!baseIsValid(numberStringSanitized, base)) return "msg";
+   if (!numberIsValid(numberStringSanitized)) return "msg";
+   if (!InputIsEqualSanitized(numberString, numberStringSanitized))
+      return "msg";
+   if (!baseIsValid(numberStringSanitized, base)) return "msg";
+   return numberStringSanitized;
+}
+
+function greater9Converter(digits) {
+   const a = 65;
+   const f = 70;
+   const correction = 55;
+   return digits.map((digit) => {
+      const number = digit.toString().toUpperCase().charCodeAt(0);
+      return parseInt(number >= a && number <= f ? number - correction : digit);
+   });
+}
+
+function conversorAlgorithm(digits, base) {
+   const digitsConvert = greater9Converter(digits);
+   const digitsLength = digitsConvert.length;
+   let digitsIteration = digitsConvert.length - 1;
+   let steps = [];
+   let result = bigInt(0);
+   for (let i = 0; i < digitsLength; i++) {
+      result = bigInt(result).add(
+         bigInt(digits[digitsIteration]).multiply(bigInt(base).pow(i))
+      );
+      digitsIteration--;
+   }
+   return result;
+}
+
+function base10Converter(numberString, base) {
+   const numberStringSanitized = allSanitizeNumber(numberString, base);
+
+   if (numberStringSanitized !== "msg") {
+      const digits = greater9Converter(extracDigits(numberStringSanitized));
+
+      return conversorAlgorithm(digits, base);
+   } else {
+      return "error";
+   }
+}
+
+///.value.toLocaleString("de-DE"));
+
+export { base10Converter };
