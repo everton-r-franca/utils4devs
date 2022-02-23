9f94fed34b7f6dcd2dedae47616848727d20e222
Everton França <everton.r.franca@gmail.com>
2022-02-22 23:39:58 -0300

    message implements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import toBase10 from "./utils/toBase10";
+import messages from "./utils/messages";
 
 const convert = { number: "ffff", base: 16 };
 console.log(toBase10.number(convert));
+
+messages.addError("alguma mensagem de erro");
+messages.addError("alguma OUTRA mensagem de erro");
+console.log(messages.hasErrors());
+console.log(messages.errors());
+messages.clearMessages();
+console.log(messages.hasErrors());
+console.log(messages.errors());
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,1 +1,68 @@
 // todo
+
+let messages = [];
+
+function addMessage(message) {
+   messages.push({
+      description: message.description,
+      type: message.type,
+      timestamp: new Date(),
+   });
+}
+
+function clearMessages() {
+   messages = [];
+}
+
+function searchByType(type) {
+   return messages.filter((e) => e.type === type);
+}
+
+function addError(message) {
+   return addMessage({ description: message, type: "error" });
+}
+
+function errors() {
+   return searchByType("error");
+}
+
+function hasErrors() {
+   return searchByType("error").length > 0;
+}
+
+function infos() {
+   return searchByType("info");
+}
+
+function addInfo(message) {
+   return addMessage({ description: message, type: "info" });
+}
+
+function hasInfos() {
+   return searchByType("info");
+}
+
+function warnings() {
+   return searchByType("warnings");
+}
+
+function addWarning(message) {
+   return addMessage({ description: message, type: "warning" });
+}
+
+function hasWarnings() {
+   return searchByType("warning");
+}
+
+export default {
+   addError,
+   addInfo,
+   addWarning,
+   hasErrors,
+   hasInfos,
+   hasWarnings,
+   errors,
+   infos,
+   warnings,
+   clearMessages,
+};
