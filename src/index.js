22abc9688c070647824ba9622119df933b6d8941
Everton França <everton.r.franca@gmail.com>
2022-02-23 14:57:39 -0300

    tests for implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 import toBase10 from "./utils/toBase10";
 
-const convert = { number: "1010", base: 2 };
-console.log(toBase10.number(convert));
+/***
+ * Converter decimal para binário
+ */
+const decimal = 45000000000000;
+function decimalTo2(decimal) {
+	const bin = [];
+	const step = [];
+	if (decimal >= 0 && decimal <= 1) return decimal;
+	for (let i = decimal; i >= 1; i /= 2) {
+		const mod = i % 2;
+		if (mod == 1) i--;
+		bin.push(mod);
+		step.push(`${i} / 2 = ${parseInt(i / 2)} with ${mod}`);
+	}
+	//console.log(step);
+	const binario = bin.reverse().toString().replace(/[,]/gi, "");
+	return binario;
+}
+
+/***
+ * Converter decimal para binário
+ */
+
+console.log(`decimal ${decimal} to binary ${decimalTo2(decimal)}`);
+
+const convert = { number: decimalTo2(decimal), base: 2 };
+
+const decimalAgain = toBase10.number(convert);
+
+console.log(`binary ${decimalTo2(decimal)} to decimal ${decimalAgain}`);
+
+/***
+ * Converter decimal para binário
+ */
+
+const hexadecimal = "fedcba987654321";
+const digits = hexadecimal.split("");
+const result = digits.map((e) => {
+	let bin = decimalTo2(toBase10.number({ number: e, base: 16 })).toString();
+	if (bin.length < 4)
+		return Math.pow(10, 4 - bin.length)
+			.toString()
+			.substring(1, 5 - bin.length)
+			.concat(bin);
+	return bin;
+});
+console.log(result.toString().replace(/[,]/gi, ""));
--- src/utils/toBase10.js
+++ src/utils/toBase10.js
@@ -2,90 +2,100 @@ import bigInt from "big-integer";
 import messages from "./messages";
 
 function toBase10(convert) {
-   const { number, base } = convert;
+	const { number, base } = convert;
 
-   const numberStringSanitized = allSanitizeNumber(number, base);
+	const numberStringSanitized = allSanitizeNumber(number, base);
 
-   if (!messages.hasErrors()) {
-      const digits = greater9Converter(extractDigits(numberStringSanitized));
-      return conversorAlgorithm(digits, base).value.toString();
-   } else {
-      return messages.errors();
-   }
+	if (!messages.hasErrors()) {
+		const digits = greater9Converter(
+			extractDigits(numberStringSanitized)
+		);
+		return conversorAlgorithm(digits, base).value.toString();
+	} else {
+		return messages.errors();
+	}
 }
 
 function allSanitizeNumber(numberString, base) {
-   const numberStringSanitized = sanitizeNumber(numberString);
-   if (!baseIsValid(base))
-      return messages.addError("A base numérica deve estar entre 2 e 16.");
-   if (!numberIsValid(numberStringSanitized))
-      return messages.addError("O deve ter no mínimo 1 digito e no máximo 1M");
-   if (!numberBaseIsValid(numberStringSanitized, base))
-      return messages.addError(
-         `${numberString} não pode pertencer a base ${base}`
-      );
+	const numberStringSanitized = sanitizeNumber(numberString);
+	if (!baseIsValid(base))
+		return messages.addError("A base numérica deve estar entre 2 e 16.");
+	if (!numberIsValid(numberStringSanitized))
+		return messages.addError(
+			"O deve ter no mínimo 1 digito e no máximo 1M"
+		);
+	if (!numberBaseIsValid(numberStringSanitized, base))
+		return messages.addError(
+			`${numberString} não pode pertencer a base ${base}`
+		);
 
-   if (!InputIsEqualSanitized(numberString, numberStringSanitized))
-      messages.addWarning("Caracteres inválidos foram removidos");
-   return numberStringSanitized;
+	if (!InputIsEqualSanitized(numberString, numberStringSanitized))
+		messages.addWarning("Caracteres inválidos foram removidos");
+	return numberStringSanitized;
 }
 
 function extractDigits(numberString) {
-   return numberString.toString().split("");
+	return numberString.toString().split("");
 }
 
 function conversorAlgorithm(digits, base) {
-   const digitsConvert = greater9Converter(digits);
-   const digitsLength = digitsConvert.length;
-   let digitsIteration = digitsConvert.length - 1;
-   let steps = [];
-   let result = bigInt(0);
-   for (let i = 0; i < digitsLength; i++) {
-      steps.push(`${digits[digitsIteration]}x${base}^${i}`);
-      result = bigInt(result).add(
-         bigInt(digits[digitsIteration]).multiply(bigInt(base).pow(i))
-      );
-      digitsIteration--;
-   }
-   console.log(steps);
-   return result;
+	const digitsConvert = greater9Converter(digits);
+	const digitsLength = digitsConvert.length;
+	let digitsIteration = digitsConvert.length - 1;
+	let steps = [];
+	let result = bigInt(0);
+	for (let i = 0; i < digitsLength; i++) {
+		result = bigInt(result).add(
+			bigInt(digits[digitsIteration]).multiply(bigInt(base).pow(i))
+		);
+		steps.push(
+			`${digits[digitsIteration]}x${base}^${i} = ${bigInt(
+				digits[digitsIteration]
+			).multiply(bigInt(base).pow(i))}`
+		);
+		digitsIteration--;
+	}
+	console.log(steps);
+	return result;
 }
 
 function sanitizeNumber(numberString) {
-   return numberString.replace(/[^0-9a-f]/gi, "");
+	return numberString.replace(/[^0-9a-f]/gi, "");
 }
 
 function baseIsValid(base) {
-   return base > 1 && base <= 16;
+	return base > 1 && base <= 16;
 }
 
 function numberIsValid(numberStringSanitized) {
-   return (
-      numberStringSanitized.length > 0 &&
-      numberStringSanitized.length <= 1000000
-   );
+	return (
+		numberStringSanitized.length > 0 &&
+		numberStringSanitized.length <= 1000000
+	);
 }
 
 function numberBaseIsValid(numberStringSanitized, base) {
-   const digits = greater9Converter(
-      extractDigits(sanitizeNumber(numberStringSanitized.toString()))
-   );
-   const max = Math.max(...digits);
-   return max >= 0 && max <= base - 1;
+	const digits = greater9Converter(
+		extractDigits(sanitizeNumber(numberStringSanitized.toString()))
+	);
+	const max = Math.max(...digits);
+	return max >= 0 && max <= base - 1;
 }
 
 function InputIsEqualSanitized(StringInput, StringSanitized) {
-   return StringInput === StringSanitized;
+	return StringInput === StringSanitized;
 }
 
 function greater9Converter(digits) {
-   const a = 65;
-   const f = 70;
-   const correction = 55;
-   return digits.map((digit) => {
-      const number = digit.toString().toUpperCase().charCodeAt(0);
-      return parseInt(number >= a && number <= f ? number - correction : digit);
-   });
+	const a = 65;
+	const f = 70;
+	const correction = 55;
+	return digits.map((digit) => {
+		const number = digit.toString().toUpperCase().charCodeAt(0);
+		return parseInt(
+			number >= a && number <= f ? number - correction : digit
+		);
+	});
 }
 
 ///.value.toLocaleString("de-DE"));
